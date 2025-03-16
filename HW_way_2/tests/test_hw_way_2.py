90bd78518db51e4c9ecd820de0af9af78cb4c0a7
Goryaeva Kseniya <goryaeva_ks@mail.ru>
2025-03-16 19:06:16 +0300

    hw way 2 done 01

--- HW_way_2/tests/test_hw_way_2.py
+++ HW_way_2/tests/test_hw_way_2.py
@@ -1,3 +1,4 @@
+from HW_way_2.data.users import User
 from HW_way_2.pages.registration_page import RegistrationPage
 
 
@@ -5,19 +6,19 @@ from HW_way_2.pages.registration_page import RegistrationPage
 def test_form(browser_manager):
 
     registration_page = RegistrationPage()
-
+    ksusha = User(first_name='Kseniya', last_name='Goryaeva', email='Goryaeva@mail.ru', mobile='8910123456', currentAddress='Нижний Новгород')
     registration_page.open()
 
-    registration_page.fill_first_name('Kseniya')
-    registration_page.fill_last_name('Goryaeva')
-    registration_page.email('Goryaeva@mail.ru')
+    registration_page.fill_first_name(ksusha.first_name)
+    registration_page.fill_last_name(ksusha.last_name)
+    registration_page.email(ksusha.email)
     registration_page.gender()
-    registration_page.mobile('8910123456')
+    registration_page.mobile(ksusha.mobile)
     registration_page.dateOfBirth('November', '11', '1995')
     registration_page.subject('hist')
     registration_page.hobbies()
     registration_page.picture()
-    registration_page.currentAddress('Нижний Новгород')
+    registration_page.currentAddress(ksusha.currentAddress)
     registration_page.scroll()
     registration_page.state()
     registration_page.city()
