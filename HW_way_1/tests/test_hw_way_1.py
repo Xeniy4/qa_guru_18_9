2071d87eb7a4b2038377f71afc3d103829cd715b
Goryaeva Kseniya <goryaeva_ks@mail.ru>
2025-03-16 18:11:14 +0300

    refactor test_hw_way_1.py

--- HW_way_1/tests/test_hw_way_1.py
+++ HW_way_1/tests/test_hw_way_1.py
@@ -1,9 +1,7 @@
 from HW_way_1.pages.registration_page import RegistrationPage
 
 
-
 def test_form(browser_manager):
-
     registration_page = RegistrationPage()
 
     registration_page.open()
@@ -13,16 +11,12 @@ def test_form(browser_manager):
     registration_page.email('Goryaeva@mail.ru')
     registration_page.gender()
     registration_page.mobile('8910123456')
-
     registration_page.dateOfBirth('November', '11', '1995')
-
     registration_page.subject('hist')
     registration_page.hobbies()
     registration_page.picture()
     registration_page.currentAddress('Нижний Новгород')
-
     registration_page.scroll()
-
     registration_page.state()
     registration_page.city()
     registration_page.submit()
