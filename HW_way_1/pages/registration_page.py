3d9da589e207b8638e0c16c6c8ce2272689b1304
Goryaeva Kseniya <goryaeva_ks@mail.ru>
2025-03-16 18:07:22 +0300

    first way done

--- HW_way_1/pages/registration_page.py
+++ HW_way_1/pages/registration_page.py
@@ -1,8 +1,6 @@
-import os
-from calendar import month
+from selene import browser, command, have
 
-from selene import browser, command, be, have
-from HW_way_1 import resources
+from HW_way_1.HW_way_1 import resources
 
 
 class RegistrationPage:
@@ -79,51 +77,3 @@ class RegistrationPage:
                 'Uttar Pradesh Merrut'
             )
         )
-
-
-def test_form(browser_manager):
-
-    registration_page = RegistrationPage()
-    registration_page.open()
-
-    registration_page.fill_first_name('Kseniya')
-    registration_page.fill_last_name('Goryaeva')
-
-    registration_page.email('Goryaeva@mail.ru')
-    registration_page.gender()
-    registration_page.mobile('8910123456')
-
-    registration_page.dateOfBirth('November', '11', '1995')
-
-    registration_page.subject('hist')
-
-    registration_page.hobbies()
-
-    registration_page.picture()
-
-    registration_page.currentAddress('Нижний Новгород')
-
-    registration_page.scroll()
-
-    registration_page.state()
-    registration_page.city()
-
-    registration_page.submit()
-
-    registration_page.finish_form()
-
-
-    # browser.element('.table').all('td').even.should(
-    #     have.exact_texts(
-    #         'Kseniya Goryaeva',
-    #         'Goryaeva@mail.ru',
-    #         'Female',
-    #         '8910123456',
-    #         '11 November,1995',
-    #         'History',
-    #         'Sports',
-    #         'pic.png',
-    #         'Нижний Новгород',
-    #         'Uttar Pradesh Merrut'
-    #     )
-    # )
